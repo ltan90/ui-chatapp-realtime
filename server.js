be162dcf33db364ee2e0f83bab8b4a4008bc1076
ltan <ltan.it9x@gmail.com>
2020-09-22 11:14:22 +0700

    Move add

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
@@ -7,6 +8,8 @@ const { addUser, removeUser, getUser }  = require('./utils/users');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const botName = 'ChatBot';
 //Run when client connects
@@ -65,5 +68,5 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
